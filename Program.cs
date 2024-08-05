3b066b3d493a5e35f6f9f61bd0c668bf8fba66ff
effdog <selfnba@gmail.com>
2024-08-06 02:37:14 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace singleton_demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance;
+            singleton1.DoSomething();
+
+            Singleton singleton2 = Singleton.GetInstance;
+            singleton2.DoSomething();
+
+            Console.WriteLine(singleton1.Equals(singleton2));
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("hello from singleton");
+        }
+
+
+    }
+}
